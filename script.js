7e33f28830a489c1d1a75463dea77b8b98496b21
Hassan Sarhan <hasoun7772@gmail.com>
2018-12-27 20:18:40 -0500

    Added some local storage functionality

---
+++ script.js
@@ -0,0 +1,97 @@
+const task = document.querySelector(".new-task");
+const tasks = document.querySelector("#tasks");
+const addBtn = document.querySelector(".bg-aquamarine");
+const clearBtn = document.querySelector(".btn-dark");
+const filter = document.querySelector(".filter-task");
+let counter = 0;
+let i = 0;
+let storedTasks;
+
+if (localStorage.getItem("Tasks") == null) {
+	storedTasks = [];
+} else {
+	storedTasks = JSON.parse(localStorage.getItem("Tasks"));
+	storedTasks.forEach(Task => {
+		counter += 1;
+		console.log(Task);
+		let t = document.createElement("li");
+		t.setAttribute("class", `list-group-item item-${counter}`);
+		t.textContent = Task;
+		const deleteButton = document.createElement("a");
+		const cross = document.createElement("i");
+		deleteButton.setAttribute("href", "#");
+		cross.setAttribute("class", "fas fa-times");
+		cross.setAttribute("style", "color:red; float:right");
+		tasks.appendChild(t);
+		t.appendChild(deleteButton);
+		deleteButton.appendChild(cross);
+		deleteList(counter);
+	});
+}
+
+addBtn.addEventListener("click", e => {
+	e.preventDefault();
+	if (task.value !== "") {
+		counter += 1;
+		const newTask = document.createElement("li");
+		newTask.setAttribute("class", `list-group-item item-${counter}`);
+
+		storedTasks.push(task.value);
+		localStorage.setItem("Tasks", JSON.stringify(storedTasks));
+		newTask.textContent = task.value;
+		const deleteButton = document.createElement("a");
+		const cross = document.createElement("i");
+		deleteButton.setAttribute("href", "#");
+		cross.setAttribute("class", "fas fa-times");
+		cross.setAttribute("style", "color:red; float:right");
+		tasks.appendChild(newTask);
+		newTask.appendChild(deleteButton);
+		deleteButton.appendChild(cross);
+		task.value = "";
+		deleteList(counter);
+		const listOfTasks = document.querySelectorAll(".list-group-item");
+		filter.addEventListener("input", e => {
+			e.preventDefault;
+			console.log(listOfTasks);
+			for (let j = 0; j < listOfTasks.length; j++) {
+				listOfTasks[j].style.display = "none";
+				console.log(listOfTasks[j]);
+				console.log(filter.value);
+				if (listOfTasks[j].textContent.includes(filter.value)) {
+					listOfTasks[j].style.display = "block";
+				}
+			}
+		});
+	}
+});
+
+filter.onkeypress = function(e) {
+	var key = e.charCode || e.keyCode || 0;
+	if (key == 13) {
+		e.preventDefault();
+	}
+};
+
+clearBtn.addEventListener("click", e => {
+	e.preventDefault();
+	localStorage.clear();
+	const clear = document.getElementsByClassName("list-group-item");
+	while (clear.length > 0) {
+		for (var li of clear) {
+			li.remove();
+		}
+	}
+});
+
+function removeFromStorage(string) {
+	let r = localStorage.getItem("tasks");
+}
+
+function deleteList(c) {
+	const taskDel = document.querySelector(`.item-${c}`);
+	taskDel.firstElementChild.addEventListener("click", () => {
+		if (confirm("Are you sure?")) {
+			taskDel.remove();
+		}
+	});
+}
